64fda3e4c6c7d99604864976025c3aa435854386
yk-a111y <yk19980811@163.com>
2024-02-21 00:15:28 +0800

    feat: 处理element

--- example/HelloWorld/App.js
+++ example/HelloWorld/App.js
@@ -3,7 +3,14 @@ import { h } from '../../lib/guide-mini-vue.esm.js'
 export const App = {
   // 1. render 编译模板成 template
   render () {
-    return h('div', 'hi, ' + this.msg);
+    return h(
+      'div', 
+      {id: 'yk-test'}, 
+      [
+        h('div', {id: 'inner'},'hi, mini-vue'), 
+        h('p', { class: 'red'}, '红色文字')
+      ]
+    );
   },
   // 2. setup (composition APIs)
   setup () {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vNode, container) {
@@ -6,14 +7,49 @@ export function render(vNode, container) {
 }
 
 function patch(vNode, container) {
-  // 组件处理
-  processComponent(vNode, container);
+  if (typeof vNode.type === 'string') {
+    // 处理元素
+    processElement(vNode, container);
+  } else if (isObject(vNode.type)) {
+    // 处理组件
+    processComponent(vNode, container);
+  }
+}
 
-  // TODO processElement
+function processElement(vNode, container) {
+  mountElement(vNode, container);
 }
 
 function processComponent(vNode, container) {
   mountComponent(vNode, container);
+}
+
+function mountElement(vNode, container) {
+  const el = document.createElement(vNode.type);
+
+  // 配置props
+  const { props } = vNode;
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  // 配置children(可能是文本，也可能是数组内嵌套多个vNode)
+  const { children } = vNode;
+  if(typeof children === 'string') {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vNode, el);
+  }
+
+  // el加入容器中
+  container.append(el);
+}
+
+function mountChildren(vNode, container) {
+  vNode.children.forEach( v => {
+    patch(v, container);
+  });
 }
 
 function mountComponent(vNode: any, container) {
@@ -25,7 +61,7 @@ function mountComponent(vNode: any, container) {
 }
 
 function setupRenderEffect(instance: any, container) {
-  const subTree = instance.render;
+  const subTree = instance.render();
 
   patch(subTree, container);
 }
